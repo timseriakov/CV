c331ae78a17ae6c696d93fc0e6e00fa169928b30
Timofei.Seriakov <t.seriakov@gmail.com>
2025-08-13 21:10:06 +0300

    chore(package): update package.json scripts for PDF generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   vite: {
     ssr: {
-      external: ['svgo'],
+      external: ["svgo"],
     },
+  },
+  devToolbar: {
+    enabled: false,
   },
 });
